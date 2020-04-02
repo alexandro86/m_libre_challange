b45c5deaa04e93bfcdf07b5f0ac689d5bfde24d7
alexandro86 <58896318+alexandro86@users.noreply.github.com>
2020-04-02 14:19:28 -0300

    change message

--- client/src/components/items/page.js
+++ client/src/components/items/page.js
@@ -5,7 +5,7 @@ import _ from "lodash";
 
 const Page = ({ items, loading }) => {
   if (_.isNull(items)) {
-    return <h1>404 Not founded!</h1>
+    return <h1>Not founded!</h1>
   } else if (!_.isEmpty(items)) {
     return (
       <div className="items">
